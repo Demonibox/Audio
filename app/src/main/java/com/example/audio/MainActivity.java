07d1cd0f51a34ab6067e4f641fe5db87f8405061
Demonibox <demonibox@gmail.com>
2020-11-21 01:59:48 +0300

    1 step

--- app/src/main/java/com/example/audio/MainActivity.java
+++ app/src/main/java/com/example/audio/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.audio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.sound);
+
+    }
+
+    public void play(View view) {
+        mediaPlayer.start();
+    }
+
+    public void pause(View view) {
+        mediaPlayer.pause();
+
     }
 }
